3e116b4d3726cac9bfc4b2b6184e79660400895d
Chris Su <chrissuusc@gmail.com>
2016-05-25 21:28:56 -0700

    add password change

--- FeedReader/FeedReader/urls.py
+++ FeedReader/FeedReader/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     url(r'^$', login.home, name='home'),
     url(r'^signUp/$', login.signUp, name='signup'),
     # url(r'^blog/', include('blog.urls')),
+    url(r'^change_password/$',login.passwordChange,name = 'passwordchange'),
     url(r'^logout/$','django.contrib.auth.views.logout',{'next_page': '/'}),
     url(r'^admin/', include(admin.site.urls)),
 ]
--- FeedReader/login/views.py
+++ FeedReader/login/views.py
@@ -8,3 +8,24 @@ def home(request):
 	return render(request,'login/home.html')
 def signUp(request):
 	return render(request,'login/signup.html')
+
+def passwordChange(request):
+    if request.method == 'POST':
+        usernames = request.POST.get('username')
+        oldPassword = request.POST.get('oldpass')
+        newPassword = request.POST.get('newpass')
+        try:
+            user = User.objects.get(username=usernames)
+        except Exception:
+            # return render(request,'registration/WrongUserOrPassword.html')
+            return HttpResponse('<p> wrong user or passwd</p>')
+        if not user.check_password(oldPassword):
+            # return render(request,'registration/WrongUserOrPassword.html')  
+            return HttpResponse('<p> wrong user or passwd</p>')
+        else:
+            user.set_password(newPassword)
+            user.save();
+            # return render(request,'registration/SuccessPasswdChange.html')  
+            return HttpResponse('<p> ok. password changed/p>')
+    else:
+        return render(request,'login/passwordChange.html');
